54cf2d68dbed78ee327ff2607ce344f0abe214fa
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-05-17 08:10:36 +0200

    .

--- src/routes/api/get/+server.js
+++ src/routes/api/get/+server.js
@@ -1,5 +1,5 @@
 import { global_counter } from '$lib/index.js'
 export function GET({ url }) {
     global_counter.x++
-    return new Response(JSON.stringify({x: 1, url, c: global_counter.x}))
+    return new Response(JSON.stringify({dt: new Date(), url, c: global_counter.x}))
 }
