18815017cb5fadf2b16fa929123656c8f67ce388
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-05-17 20:14:29 +0200

    .

--- src/routes/api/+page.server.js
+++ src/routes/api/+page.server.js
@@ -1,8 +1,12 @@
+import Database from 'better-sqlite3'
+const db = new Database('mydb.db', { verbose: console.log })
 export const actions = {
 	login: async ({ request, cookies }) => {
         const data = await request.formData()
         var un = data.get('username')
-        cookies.set('username', un, { path: '/' })
+        var pw = data.get('password')
+        const res = db.prepare('SELECT * FROM users WHERE email = ? and password_hash = ?').get(un, pw)
+        if (res?.email) cookies.set('username', res.email, { path: '/' })
     },
     logout: async ({ cookies }) => {
         cookies.delete('username', { path: '/' })
