7ba4f5f3c8496da69d1384d766dcad2b853650b7
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-05-17 15:33:30 +0200

    .

--- src/routes/content/+page.server.js
+++ src/routes/content/+page.server.js
@@ -1,10 +1,13 @@
+import Database from 'better-sqlite3'
+const db = new Database('mydb.db')
 import { global_counter } from '$lib/index.js'
 var counter = 0
 export function load({ cookies }) {
   global_counter.x++
   var un = cookies.get('username')
+  const res = db.prepare('SELECT * FROM users').all()
   return {
-    c_user: un,
+    c_user: un, res, 
     titkos_tartalom: `Hello ${un}, a számláló állása: ${counter++}`,
     gc: global_counter.x
   }       
--- src/routes/content/+page.svelte
+++ src/routes/content/+page.svelte
@@ -5,7 +5,15 @@
 <h1>Content</h1>
 {data.c_user}
 <hr>
-Titkos tartalom:
+<h3>Összes felhasználó</h3>
+<div class="c">
+{#each data.res as row}
+    <span>
+        <div class="name">{row.name}</div>
+        <div>{row.email}</div>
+    </span>
+{/each}
+</div>
 <br><br>
 {data.titkos_tartalom}
 <hr>
@@ -22,5 +30,31 @@ Tartalom nem hozzáférhető
     i {
         color: #a73945;
         text-shadow: 1px 1px 3px gray;
+    }
+    .c {
+        display: inline-flex;
+        flex-wrap: wrap;
+    }
+    span {
+        white-space: nowrap;
+        display: inline-block;
+        background-color: rgb(174, 197, 217);
+        border: solid 1px gray;
+        padding: 2px;
+        margin: 2px;
+        border-radius: 9px;
+        box-shadow: 1px 1px 3px black;
+    }
+    .c span div {
+        padding: 5px;
+        margin: 5px;
+        background-color: aqua;
+        border-radius: 6px;
+        box-shadow: 1px 1px 3px inset black;
+        white-space: nowrap;
+    }
+    .c span div.name {
+        background-color: black;
+        color:aliceblue
     }
 </style>
