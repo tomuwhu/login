6e5ab1734fab36605eae92dbe205bfefe718efe8
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-05-22 22:21:08 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,12 +10,52 @@
         <button>Logout</button>
     </form>
 {:else}
+    <h1>Bejelentkezés</h1>
     <form method="POST" action="/api?/login">
-        <label for="un">Felhasználónév</label>
-        <input type="text" name="username" id="un">
-        <br>
-        <label for="pw">Jelszó</label>
-        <input type="password" name="password" id="pw">
-        <button>Login</button>
+        <table>
+            <tr>
+                <td><label for="un">Felhasználónév</label></td>
+                <td><input type="text" name="username" id="un"></td>
+            </tr>
+            <tr>
+                <td><label for="pw">Jelszó</label></td>
+                <td><input type="password" name="password" id="pw"></td>
+            </tr>
+            <tr>
+                <td colspan="2" class=c><button>Login</button></td>
+            </tr>
+        </table>
     </form>
+    <br><br>
 {/if}
+<style>
+    table {
+        margin: auto;
+        border-spacing: 4px;
+        box-shadow: 1px 1px 3px black;
+        background-color: rgb(46, 96, 114);
+        padding: 2px;
+        border-radius: 10px;
+    }
+    td {
+        padding: 8px;
+        padding-left: 10px;
+        padding-right: 15px;
+        background-color: aliceblue;
+        border-radius: 6px;
+        box-shadow: 1px 1px 3px inset black;
+        text-align: left;
+    }
+    td.c {
+        text-align: center;
+    }
+    input {
+        width: 100%;
+        border: none;
+        background: transparent;
+        font-size: 13px;
+        font-family: "Playfair Display", serif;
+        font-optical-sizing: auto;
+        border-bottom: solid 1px black;
+    }
+</style>
